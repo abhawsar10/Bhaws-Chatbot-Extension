f62b283b19c3a723b9bfd58e885998e0246b9292
Ankit Bhawsar <bhawsarankit10@gmail.com>
2023-10-13 09:38:36 -0700

    Added Scroll up, down, nav

--- content.js
+++ content.js
@@ -9,6 +9,17 @@ function findButtonByText(text) {
     return null;
 }
 
+function findNavButtonByText(text) {
+    
+    let buttons = document.querySelectorAll('.contentNav__link');
+    for (let button of buttons) {
+        if (button.querySelector('span').textContent.toLowerCase().includes(text.toLowerCase())) {
+            return button;
+        }   
+    }
+    return null;
+}
+
 function add_to_cart(){
 
     let text = 'Add 1 Item to Cart'
@@ -23,13 +34,60 @@ function add_to_cart(){
 
 }
 
+function scrolldown(){
+
+    window.scrollBy({
+        top: window.innerHeight,
+        left: 0, 
+        behavior: 'smooth'
+    });
+}
+
+function scrollup(){
+
+    window.scrollBy({
+        top: -window.innerHeight,
+        left: 0, 
+        behavior: 'smooth'
+    });
+}
+
+function scrollto(scrolltonav){
+
+    let navButton = null;
+    
+    navButton = findNavButtonByText(scrolltonav);
+
+    console.log("found this element ",navButton)
+    if (navButton!==null){
+        navButton.click();
+    }else{
+        scrolldown();
+    }
+
+}
 
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+
       if (request.action == "add_to_cart") {
         add_to_cart();
-        sendResponse({result: "success"});
+        sendResponse({result: "Successfully Added to cart"});
       }
+      if (request.action == "scroll_down") {
+        scrolldown();
+        sendResponse({result: "Successfully Scrolled Down"});
+      }
+      if (request.action == "scroll_up") {
+        scrollup();
+        sendResponse({result: "Successfully Scrolled Up"});
+      }
+      if (request.action == "scroll_to") {
+        scrollto(request.scroll_to_nav);
+        sendResponse({result: "Successfully Scrolled To Nav"});
+      }
+
     }
+    
 );
